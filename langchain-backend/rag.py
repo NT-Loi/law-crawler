055636165ede1bcf8ef4ad995714936ebcf0aa4d
NT-Loi <loi27652023@gmail.com>
2026-01-11 04:00:31 +0700

    add reranker

--- langchain-backend/rag.py
+++ langchain-backend/rag.py
@@ -3,6 +3,7 @@ from qdrant_client.models import Prefetch, SparseVector, Fusion, FusionQuery, Fi
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from fastembed import SparseTextEmbedding
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 import torch
 from typing import Any
@@ -40,6 +41,13 @@ class RAG:
         logging.info(f"Initialize sparse embedding: {self.sparse_embedding}")
         
         self.pd_collection_name = get_collection_name("phapdien", model_name)
+
+        rerank_model_name = os.getenv("RERANKING_MODEL")
+        self.rerank_tokenizer = AutoTokenizer.from_pretrained(rerank_model_name)
+        self.rerank_model = AutoModelForSequenceClassification.from_pretrained(rerank_model_name)
+        self.rerank_model.to(self.device)
+        self.rerank_model.eval()
+        logging.info(f"Initialize reranker: {rerank_model_name}")
         
     def retrieve(self, query: str, top_k: int = 5) -> list[dict[str, Any]]:
         logging.info(f"Retrieve documents for query: {query}")
@@ -52,11 +60,11 @@ class RAG:
         results = self.qdrant_client.query_points(
             collection_name=self.pd_collection_name,
             prefetch=[
-                Prefetch(query=dense_vec, using="dense", limit=top_k * 3),
-                Prefetch(query=sparse_vec, using="sparse", limit=top_k * 3)
+                Prefetch(query=dense_vec, using="dense", limit=top_k * 20),
+                Prefetch(query=sparse_vec, using="sparse", limit=top_k * 20)
             ],
             query=FusionQuery(fusion=Fusion.RRF),
-            limit=top_k
+            limit=top_k * 10 # Retrieve more for reranking
         ).points
 
         sources = []
@@ -66,16 +74,39 @@ class RAG:
             content = payload.get("content")
                     
             sources.append({"id": source_id, "content": content})
-        return sources
+        
+        return self.rerank(query, sources, top_k=top_k)
 
     def rerank(self, query: str, sources: list[dict[str, Any]], top_k: int = 5) -> list[dict[str, Any]]:
         logging.info(f"Rerank documents for query: {query}")
-        pass
-        # return sources
+        
+        if not sources:
+            return []
 
-    # def build_context(self, sources: list[dict[str, Any]]) -> str:
-    #     docs_content = "\n\n".join(doc["content"] for doc in sources)
-    #     return docs_content
+        pairs = [[query, doc["content"]] for doc in sources]
+        
+        MAX_LENGTH = 2304 # 256 for query and 2048 for passages
+        
+        with torch.no_grad():
+            inputs = self.rerank_tokenizer(
+                pairs, 
+                padding=True, 
+                truncation=True, 
+                return_tensors='pt', 
+                max_length=MAX_LENGTH
+            ).to(self.device)
+            
+            scores = self.rerank_model(**inputs, return_dict=True).logits.view(-1, ).float()
+            
+        scored_sources = []
+        for i, score in enumerate(scores):
+            doc = sources[i].copy()
+            doc["score"] = score.item()
+            scored_sources.append(doc)
+            
+        scored_sources.sort(key=lambda x: x["score"], reverse=True)
+        
+        return scored_sources[:top_k]
 
     def close(self):
         self.qdrant_client.close()
